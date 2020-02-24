0748f90f1ff7a4e1ba40b71f7a8c3f37dce7653a
yangemail8 <yangemail8@gmail.com>
2020-02-24 15:19:04 +0800

    create component of paper-list.vue.

---
+++ components/paper/paper-list.vue
@@ -0,0 +1,80 @@
+<template>
+
+	<!-- 小纸条列表 -->
+	<view class="paper-list u-f-ac">
+		<!-- 左边：图标 -->
+		<image :src="item.userpic" mode="widthFix" lazy-load></image>
+		<!-- 右边： 两行 -->
+		<view>
+			<view class="u-f-ac u-f-jsb">{{item.username}}
+				<view class="u-f-ajfe">{{item.time}}</view>
+			</view>
+			<view class="u-f-ac u-f-jsb">{{item.data}}
+				<template v-if="item.noreadnum>0">
+					<uni-badge class="uni-badge-left-margin" :text="item.noreadnum" type="error" />
+				</template>
+			</view>
+		</view>
+	</view>
+
+</template>
+
+<script scoped>
+	import uniBadge from '@/components/uni-badge/uni-badge.vue';
+
+	export default {
+		components: {
+			uniBadge,
+		},
+		props: {
+			item: {
+				type: Object,
+				default: null
+			},
+			index: {
+				type: Number,
+				default: 0
+			}
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped>
+	.paper-list {
+		border-bottom: 1rpx solid #EEE;
+		padding: 20rpx 0;
+	}
+
+	.paper-list image {
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 100%;
+		margin-right: 20rpx;
+		/* flex-shrink:0 表示不收右边挤压 */
+		flex-shrink: 0;
+	}
+
+	.paper-list view {
+		/* flex:1 表示右边填充剩余的空间 */
+		flex: 1;
+	}
+
+	.paper-list>view>view:first-child {
+		font-size: 35rpx;
+		/* border: 1rpx solid; */
+	}
+
+	.paper-list>view>view:first-child>view {
+		color: #CBCBCB;
+		/* border: 1rpx solid red; */
+	}
+
+	.paper-list>view>view:last-child {
+		color: #999;
+	}
+</style>
--- pages/paper/paper.vue
+++ pages/paper/paper.vue
@@ -1,30 +1,51 @@
 <template>
 	<view class="body">
 		<!-- 小纸条列表 -->
-		<view class="paper-list u-f-ac">
-			<!-- 左边：图标 -->
-			<image src="../../static/demo/userpic/12.jpg" mode="widthFix" lazy-load></image>
-			<!-- 右边： 两行 -->
-			<view>
-				<view class="u-f-ac u-f-jsb">昵称<view class="u-f-ajfe">10:21</view></view>
-				<view class="u-f-ac u-f-jsb">我是信息
-					<uni-badge class="uni-badge-left-margin" text="123" type="error" />
-				</view>
-			</view>
-		</view>
+		<block v-for="(item, index) in list" :key="index">
+			<paper-list :item="item" :index="index"></paper-list>
+		</block>
 	</view>
 </template>
 
 <script>
-	import uniBadge from '@/components/uni-badge/uni-badge.vue';
+	import paperList from '../../components/paper/paper-list.vue';
 
 	export default {
 		components: {
-			uniBadge,
+			paperList,
 		},
 		data() {
 			return {
-
+				list:[
+					{
+						userpic:"../../static/demo/userpic/12.jpg",
+						username:"昵称1",
+						time:"10:21",
+						data:"我是信息1",
+						noreadnum:2,
+					},
+					{
+						userpic:"../../static/demo/userpic/12.jpg",
+						username:"昵称2",
+						time:"10:21",
+						data:"我是信息2",
+						noreadnum:0,
+					},
+					{
+						userpic:"../../static/demo/userpic/12.jpg",
+						username:"昵称3",
+						time:"10:21",
+						data:"我是信息3",
+						noreadnum:0,
+					},
+					{
+						userpic:"../../static/demo/userpic/12.jpg",
+						username:"昵称4",
+						time:"10:21",
+						data:"我是信息4",
+						noreadnum:11,
+					},
+				]
 			}
 		},
 		methods: {
@@ -36,38 +57,5 @@
 <style>
 	.body {
 		padding: 0 20rpx;
-	}
-
-	.paper-list {
-		border-bottom: 1rpx solid #EEE;
-		padding: 20rpx 0;
-	}
-
-	.paper-list image {
-		width: 100rpx;
-		height: 100rpx;
-		border-radius: 100%;
-		margin-right: 20rpx;
-		/* flex-shrink:0 表示不收右边挤压 */
-		flex-shrink: 0;
-	}
-
-	.paper-list view {
-		/* flex:1 表示右边填充剩余的空间 */
-		flex: 1;
-	}
-	
-	.paper-list>view>view:first-child {
-		font-size: 35rpx;
-		/* border: 1rpx solid; */
-	}
-	
-	.paper-list>view>view:first-child>view{
-		color: #CBCBCB;
-		/* border: 1rpx solid red; */
-	}
-	
-	.paper-list>view>view:last-child {
-		color: #999;
 	}
 </style>
