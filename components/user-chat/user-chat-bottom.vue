9c73fb493acc6aaed8afb7c2a64b1d4c8c90ea9a
yangemail8 <yangemail8@gmail.com>
2020-02-25 15:30:15 +0800

    #78 is done.

---
+++ components/user-chat/user-chat-bottom.vue
@@ -0,0 +1,52 @@
+<template>
+	<!-- 聊天页面底部输入框 -->
+	<view class="user-chat-bottom u-f-ac">
+		<input type="text" placeholder="文明发言" v-model="text" />
+		<view class="icon iconfont icon-fabu u-f-ajc" @tap="submit"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				text: ""
+			};
+		},
+		methods: {
+			submit() {
+				// console.log(this.text);
+				this.$emit("submit", this.text);
+				this.text = "";
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	.user-chat-bottom {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		height: 120rpx;
+		padding: 0 20rpx;
+		background: white;
+		border-top: 1rpx solid #eee;
+	}
+
+	.user-chat-bottom>input {
+		/* border: 1rpx solid red; */
+		flex: 1;
+		margin-right: 20rpx;
+		padding: 10rpx 20rpx;
+		border-radius: 10rpx;
+		background: #F7F7F7;
+	}
+
+	.user-chat-bottom>view {
+		/* background: yellow; */
+		width: 80rpx;
+		font-size: 45rpx;
+	}
+</style>
--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,22 +1,32 @@
 <template>
 	<view>
-		聊天页面
+		<!-- 聊天页面底部输入框 -->
+		<user-chat-bottom @submit="submit"></user-chat-bottom>
+		
 	</view>
 </template>
 
 <script>
+	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue"
+	
 	export default {
+		components: {
+			userChatBottom
+		},
 		data() {
 			return {
 				
 			}
 		},
 		methods: {
-			
+			// 发送留言逻辑
+			submit(data) {
+				console.log("当前输入的是：" + data);
+			}
 		}
 	}
 </script>
 
 <style>
-
+	
 </style>
