eeb098da332ef91ba71ade94fcdb4bc0c3340055
yangemail8 <yangemail8@gmail.com>
2020-02-25 21:31:28 +0800

    #81 is done.

---
+++ components/user-chat/user-chat-list.vue
@@ -0,0 +1,101 @@
+<template>
+	<!-- 聊天列表 -->
+	<view class="user-chat-item">
+		<!-- 时间 -->
+		<view v-if="item.gstime" class="user-chat-time u-f-ajc">{{item.gstime}}</view>
+		<!-- 使用u-f，因为不需要垂直方向居中 -->
+		<view class="user-chat-list u-f" :class="{'user-chat-me':(item.isme)}">
+			<!-- 好友的头像 -->
+			<image v-if="!item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
+			<view class="user-chat-list-body">
+				<!-- 文字 -->
+				<text v-if="item.type == 'text'">{{item.data}}</text>
+				<!-- 图片 -->
+				<image v-if="item.type == 'img'" :src="item.data" mode="widthFix" lazy-load></image>
+			</view>
+			<!-- 自己的头像 -->
+			<image v-if="item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: Object,
+			index: Number
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped>
+	.user-chat-time {
+		padding: 50rpx 0;
+		color: #A2A2A2;
+		font-size: 24rpx;
+	}
+
+	.user-chat-list {
+		padding: 20rpx 0;
+	}
+
+	.user-chat-list>image {
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 100%;
+		flex-shrink: 0;
+	}
+
+	.user-chat-list-body {
+		position: relative;
+		background: #F4F4F4;
+		padding: 25rpx;
+		margin-left: 20rpx;
+		/* 防止数据过长触碰到右边 */
+		margin-right: 100rpx;
+		border-radius: 20rpx;
+	}
+
+	/* :after 选择器在被选元素的内容后面插入内容。请使用 content 属性来指定要插入的内容。*/
+	.user-chat-list-body::after {
+		position: absolute;
+		left: -30rpx;
+		right: 0;
+		top: 30rpx;
+		content: '';
+		/* 需要加入width:0，否则从前到后的长度 */
+		width: 0;
+		height: 0;
+		border: 16rpx solid #F4F4F4;
+		/* 因为上面的border大小为16rpx，此处border-color为四部分组成。上、右、下、左 */
+		border-color: transparent #F4F4F4 transparent transparent;
+	}
+
+	/* .user-chat-me是我自己发送的信息样式 */
+	.user-chat-me {
+		justify-content: flex-end;
+	}
+
+	/* 表示：.user-chat-me下面的.user-chat-list-body */
+	.user-chat-me .user-chat-list-body {
+		margin-right: 20rpx;
+		margin-left: 100rpx;
+	}
+
+	.user-chat-me .user-chat-list-body::after {
+		/* 默认值。通过浏览器计算左边缘的位置。 */
+		left: auto;
+		right: -30rpx;
+		border-color: transparent transparent transparent #F4F4F4;
+	}
+
+	.user-chat-list-body>image {
+		max-width: 150rpx;
+		max-height: 200rpx;
+	}
+</style>
--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,23 +1,11 @@
 <template>
 	<view>
 		<!-- 聊天列表 -->
-		<block v-for="(item, index) in list" :key="index">
-			<!-- 时间 -->
-			<view v-if="item.gstime" class="user-chat-time u-f-ajc">{{item.gstime}}</view>
-			<!-- 使用u-f，因为不需要垂直方向居中 -->
-			<view class="user-chat-list u-f" :class="{'user-chat-me':(item.isme)}">
-				<!-- 好友的头像 -->
-				<image v-if="!item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
-				<view class="user-chat-list-body">
-					<!-- 文字 -->
-					<text v-if="item.type == 'text'">{{item.data}}</text>
-					<!-- 图片 -->
-					<image v-if="item.type == 'img'" :src="item.data" mode="widthFix" lazy-load></image>
-				</view>
-				<!-- 自己的头像 -->
-				<image v-if="item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
-			</view>
-		</block>
+		<scroll-view id="scrollview" scroll-y :scroll-top="scrollTop" :scroll-with-animation="true" :style="{height: style.contentHeight+'px'}">
+			<block v-for="(item, index) in list" :key="index">
+				<user-chat-list :item="item" :index="index"></user-chat-list>
+			</block>
+		</scroll-view>
 
 		<!-- 聊天页面底部输入框 -->
 		<user-chat-bottom @submit="submit"></user-chat-bottom>
@@ -26,31 +14,65 @@
 </template>
 
 <script>
+	// 时间公用函数
+	import time from "../../common/time.js";
+	// 用户聊天列表
+	import userChatList from "../../components/user-chat/user-chat-list.vue";
 	// 底部输入框
-	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue"
-	// 时间功用函数
-	import time from "../../common/time.js"
+	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue";
 
 	export default {
 		components: {
+			userChatList,
 			userChatBottom
 		},
 		data() {
 			return {
+				scrollTop: 0,
+				style: {
+					contentHeight: 0,
+					itemHeight: 0
+				},
 				list: []
 			}
 		},
 		onLoad() {
-			// let s1 = "1554970014";
-			// let s2 = "1555146414";
-			// let s3 = (new Date()).getTime();
-			// console.log(time.gettime.gettime(s1));
-			// console.log(time.gettime.gettime(s2));
-			// console.log(time.gettime.gettime(s3));
 			// 从服务器获取数据，一定要在onLoad(){}中，调用一下
 			this.getData();
+			// 调用计算屏幕高度
+			this.initdata();
+		},
+		onReady() {
+			// 获取scrollTop位置信息
+			this.pageToBottom();
 		},
 		methods: {
+			// 初始化参数
+			initdata() {
+				try {
+					const res = uni.getSystemInfoSync();
+					this.style.contentHeight = res.windowHeight - uni.upx2px(120);
+				} catch (e) {
+					//TODO handle the exception
+				}
+			},
+			pageToBottom() {
+				let q = uni.createSelectorQuery();
+				q.select('#scrollview').boundingClientRect(); // 这个有什么用呢？
+				q.selectAll('.user-chat-time').boundingClientRect();
+
+				q.exec((res) => {
+					// console.log("res: " + JSON.stringify(res));
+					res[1].forEach((val) => {
+						this.style.itemHeight += val.height;
+					});
+
+					if (this.style.itemHeight > this.style.contentHeight) {
+						this.scrollTop = this.style.itemHeight;
+						// console.log("this.scrollTop: " + this.scrollTop)
+					}
+				});
+			},
 			// 聊天数据
 			getData() {
 				// 模拟：从服务器获取到的数据
@@ -80,74 +102,31 @@
 			// 发送留言逻辑
 			submit(data) {
 				console.log("当前输入的是：" + data);
+				// 构建数据
+				let now = new Date().getTime();
+				let lasttime = 0;
+				if (this.list.length > 0) {
+					lasttime = this.list[this.list.length - 1].time;
+				}
+
+				let obj = {
+					isme: true, // 发送输入框，肯定是自己发送的，所以为true
+					userpic: "../../static/demo/userpic/10.jpg",
+					type: "text",
+					data: data,
+					time: now,
+					gstime: time.gettime.getChatTime(now, this.list[this.list.length - 1].time)
+				};
+
+				this.list.push(obj);
+
+				// 每次提交之后，重新计算底部滚动条位置
+				this.pageToBottom();
 			}
 		}
 	}
 </script>
 
 <style>
-	.user-chat-time {
-		padding: 50rpx 0;
-		color: #A2A2A2;
-		font-size: 24rpx;
-	}
 
-	.user-chat-list {
-		padding: 20rpx 0;
-	}
-
-	.user-chat-list>image {
-		width: 100rpx;
-		height: 100rpx;
-		border-radius: 100%;
-		flex-shrink: 0;
-	}
-
-	.user-chat-list-body {
-		position: relative;
-		background: #F4F4F4;
-		padding: 25rpx;
-		margin-left: 20rpx;
-		/* 防止数据过长触碰到右边 */
-		margin-right: 100rpx;
-		border-radius: 20rpx;
-	}
-
-	/* :after 选择器在被选元素的内容后面插入内容。请使用 content 属性来指定要插入的内容。*/
-	.user-chat-list-body::after {
-		position: absolute;
-		left: -30rpx;
-		right: 0;
-		top: 30rpx;
-		content: '';
-		/* 需要加入width:0，否则从前到后的长度 */
-		width: 0;
-		height: 0;
-		border: 16rpx solid #F4F4F4;
-		/* 因为上面的border大小为16rpx，此处border-color为四部分组成。上、右、下、左 */
-		border-color: transparent #F4F4F4 transparent transparent;
-	}
-
-	/* .user-chat-me是我自己发送的信息样式 */
-	.user-chat-me {
-		justify-content: flex-end;
-	}
-
-	/* 表示：.user-chat-me下面的.user-chat-list-body */
-	.user-chat-me .user-chat-list-body {
-		margin-right: 20rpx;
-		margin-left: 100rpx;
-	}
-
-	.user-chat-me .user-chat-list-body::after {
-		/* 默认值。通过浏览器计算左边缘的位置。 */
-		left: auto;
-		right: -30rpx;
-		border-color: transparent transparent transparent #F4F4F4;
-	}
-
-	.user-chat-list-body>image {
-		max-width: 150rpx;
-		max-height: 200rpx;
-	}
 </style>
