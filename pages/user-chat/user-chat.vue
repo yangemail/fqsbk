d257b18be7d0ec28aaa272df5178d943f68b95d0
yangemail8 <yangemail8@gmail.com>
2020-02-25 20:04:04 +0800

    commit before move to new component user-chat-list.vue

--- pages/user-chat/user-chat.vue
+++ pages/user-chat/user-chat.vue
@@ -1,19 +1,23 @@
 <template>
 	<view>
-
 		<!-- 聊天列表 -->
-		<!-- 使用u-f，因为不需要垂直方向居中 -->
-		<view class="user-chat-list u-f user-chat-me">
-			<!-- 头像 -->
-			<!-- <image src="../../static/demo/userpic/11.jpg" mode="widthFix" lazy-load></image> -->
-			<view class="user-chat-list-body">
-				<!-- 文字 -->
-				<!-- <text>随便随便随便随便随便随便</text> -->
-				<!-- 图片 -->
-				<image src="../../static/demo/3.jpg" mode="widthFix" lazy-load></image>
+		<block v-for="(item, index) in list" :key="index">
+			<!-- 时间 -->
+			<view v-if="item.gstime" class="user-chat-time u-f-ajc">{{item.gstime}}</view>
+			<!-- 使用u-f，因为不需要垂直方向居中 -->
+			<view class="user-chat-list u-f" :class="{'user-chat-me':(item.isme)}">
+				<!-- 好友的头像 -->
+				<image v-if="!item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
+				<view class="user-chat-list-body">
+					<!-- 文字 -->
+					<text v-if="item.type == 'text'">{{item.data}}</text>
+					<!-- 图片 -->
+					<image v-if="item.type == 'img'" :src="item.data" mode="widthFix" lazy-load></image>
+				</view>
+				<!-- 自己的头像 -->
+				<image v-if="item.isme" :src="item.userpic" mode="widthFix" lazy-load></image>
 			</view>
-			<image src="../../static/demo/userpic/11.jpg" mode="widthFix" lazy-load></image>
-		</view>
+		</block>
 
 		<!-- 聊天页面底部输入框 -->
 		<user-chat-bottom @submit="submit"></user-chat-bottom>
@@ -22,7 +26,10 @@
 </template>
 
 <script>
+	// 底部输入框
 	import userChatBottom from "../../components/user-chat/user-chat-bottom.vue"
+	// 时间功用函数
+	import time from "../../common/time.js"
 
 	export default {
 		components: {
@@ -30,10 +37,46 @@
 		},
 		data() {
 			return {
-
+				list: []
 			}
 		},
+		onLoad() {
+			// let s1 = "1554970014";
+			// let s2 = "1555146414";
+			// let s3 = (new Date()).getTime();
+			// console.log(time.gettime.gettime(s1));
+			// console.log(time.gettime.gettime(s2));
+			// console.log(time.gettime.gettime(s3));
+			// 从服务器获取数据，一定要在onLoad(){}中，调用一下
+			this.getData();
+		},
 		methods: {
+			// 聊天数据
+			getData() {
+				// 模拟：从服务器获取到的数据
+				let arr = [{
+						isme: false,
+						userpic: "../../static/demo/userpic/11.jpg",
+						type: "text",
+						data: "哈哈哈",
+						time: "1554970014",
+						gstime: "" // 转化之后的时间格式，例如：12:00
+					},
+					{
+						isme: true,
+						userpic: "../../static/demo/userpic/10.jpg",
+						type: "img",
+						data: "../../static/demo/3.jpg",
+						time: "1555146414",
+						gstime: ""
+					}
+				];
+				for (let i = 0; i < arr.length; i++) {
+					// getChatTime，信息时间小于300秒（5分钟），则不重复显示时间
+					arr[i].gstime = time.gettime.getChatTime(arr[i].time, i > 0 ? arr[i - 1].time : 0);
+				}
+				this.list = arr;
+			},
 			// 发送留言逻辑
 			submit(data) {
 				console.log("当前输入的是：" + data);
@@ -43,7 +86,15 @@
 </script>
 
 <style>
-	.user-chat-list {}
+	.user-chat-time {
+		padding: 50rpx 0;
+		color: #A2A2A2;
+		font-size: 24rpx;
+	}
+
+	.user-chat-list {
+		padding: 20rpx 0;
+	}
 
 	.user-chat-list>image {
 		width: 100rpx;
@@ -94,8 +145,8 @@
 		right: -30rpx;
 		border-color: transparent transparent transparent #F4F4F4;
 	}
-	
-	.user-chat-list-body >image {
+
+	.user-chat-list-body>image {
 		max-width: 150rpx;
 		max-height: 200rpx;
 	}
