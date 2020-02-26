dd072f1ce33169f5cb3e5f66d8b943071c66ee08
yangemail8 <yangemail8@gmail.com>
2020-02-26 17:25:38 +0800

    #86 is done.

---
+++ components/detail/comment-list.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<!-- 评论列表 -->
+		<view class="uni-comment-list " :class="{'u-comment-list-child':(item.fid>0)}">
+			<view class="uni-comment-face">
+				<image :src="item.userpic" mode="widthFix"></image>
+			</view>
+			<view class="uni-comment-body">
+				<view class="uni-comment-top">
+					<text>{{item.username}}</text>
+				</view>
+				<view class="uni-comment-content">{{item.data}}</view>
+				<view class="uni-comment-date">
+					<view>{{item.time}}</view>
+					<!-- <view class="uni-comment-replay-btn">5回复</view> -->
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: {
+			item: Object,
+			index: Number
+		},
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style scoped>
+	.u-comment-list-child {
+		padding: 20rpx;
+		background: #F4F4F4;
+		border-bottom: 1rpx solid #EEE;
+		/* box-sizing 以特定的方式定义匹配某个区域的特定元素。
+		通过将 box-sizing 设置为 "border-box"。这可令浏览器呈现出带有指定宽度和高度的框，并把边框和内边距放入框中。 */
+		box-sizing: border-box;
+		/* 将公共css中的外边距设为0 */
+		margin: 0;
+		/* 然后再更改margin-left为70rpx */
+		margin-left: 70rpx;
+		/* 确保元素不会超出范围 */
+		width: auto;
+	}
+</style>
--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,15 +1,31 @@
 <template>
 	<view>
+
+		<!-- 详情列表 -->
 		<detail-info :item="detail"></detail-info>
+
+		<!-- 评论列表 -->
+		<view class="u-comment-title">最新评论 {{comment.count}}</view>
+		<view class="uni-comment u-comment">
+			<block v-for="(item, index) in comment.list" :key="index">
+				<comment-list :item="item" :index="index"></comment-list>
+			</block>
+		</view>
 	</view>
 </template>
 
 <script>
-	import detailInfo from "../../components/detail/detail-info.vue"
+	// 详情列表
+	import detailInfo from "../../components/detail/detail-info.vue";
+	// 时间公用方法
+	import time from "../../common/time.js";
+	// 评论列表
+	import commentList from "../../components/detail/comment-list.vue";
 
 	export default {
 		components: {
-			detailInfo
+			detailInfo,
+			commentList
 		},
 		data() {
 			return {
@@ -32,11 +48,20 @@
 					commentnum: 30,
 					goodnum: 20,
 				},
+				comment: {
+					count: 20, // 数据库中评论的总数，并不是本数组获取到的数量
+					list: []
+				}
 			}
 		},
 		onLoad(e) {
 			console.log("--- onLoad(e) is called. ---")
+
+			// 获取前一个页面传入的JSON数据
 			this.initdata(JSON.parse(e.detailData)); // 将首页传入的JSON字符串，转换为Object
+
+			// 初始化数据
+			this.getcomment();
 		},
 		// 监听导航右边按钮点击事件
 		onNavigationBarButtonTap(e) {
@@ -51,11 +76,63 @@
 				uni.setNavigationBarTitle({
 					title: obj.title
 				});
+			},
+			getcomment() {
+				let arr = [
+					// 一级评论
+					{
+						id: 1,
+						fid: 0, // 父级评论ID，如果是1级评论，默认为0
+						userpic: "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png",
+						username: "小猫咪",
+						time: "1555400035",
+						data: "支持国产，支持DCloud!"
+					},
+					// 子级评论
+					{
+						id: 2,
+						fid: 1, // 父级评论ID，如果是1级评论，默认为0
+						userpic: "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png",
+						username: "小猫咪",
+						time: "1555400035",
+						data: "支持国产，支持DCloud!"
+					},
+					// 子级评论
+					{
+						id: 3,
+						fid: 1, // 父级评论ID，如果是1级评论，默认为0
+						userpic: "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png",
+						username: "小猫咪",
+						time: "1555400035",
+						data: "支持国产，支持DCloud!"
+					},
+					// 一级评论
+					{
+						id: 4,
+						fid: 0, // 父级评论ID，如果是1级评论，默认为0
+						userpic: "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/uni@2x.png",
+						username: "小猫咪",
+						time: "1555400035",
+						data: "支持国产，支持DCloud!"
+					},
+				];
+				for (let i = 0; i < arr.length; i++) {
+					arr[i].time = time.gettime.gettime(arr[i].time);
+				}
+				this.comment.list = arr;
 			}
 		}
 	}
 </script>
 
 <style>
+	.u-comment-title {
+		padding: 20rpx;
+		font-size: 30rpx;
+		font-weight: bold;
+	}
 
+	.u-comment {
+		padding: 0 20rpx;
+	}
 </style>
