a618798294a2fa2adeec8532f3fb1e1d0d4b4399
yangemail8 <yangemail8@gmail.com>
2020-02-27 15:23:37 +0800

    #90 is done.

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,11 +8,21 @@
 	export default {
 		data() {
 			return {
-				
+
+			}
+		},
+		onNavigationBarButtonTap(e) {
+			console.log("Menu button is tapped (index): " + e.index);
+			switch (e.index) {
+				case 0:
+					console.log("跳转到设置页面");
+					break;
+				default:
+					break;
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
