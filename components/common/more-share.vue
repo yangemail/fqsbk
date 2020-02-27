2153a7b853f1fe58bca43d90a643e1071cf3087f
yangemail8 <yangemail8@gmail.com>
2020-02-27 14:56:33 +0800

    #89 is done. (BUT with bug in share channels)

--- components/common/more-share.vue
+++ components/common/more-share.vue
@@ -1,32 +1,20 @@
 <template>
+	<!-- TODO: 分享到不工作！！！！ -->
+	
 	<view class="animated fadeIn faster" v-if="show">
 		<!-- 分享更多功能 -->
 		<!-- 这个view标签是蒙版 -->
-		<view class="more-share-model"  @tap="toggle"></view>
+		<view class="more-share-model" @tap="toggle"></view>
 		<!-- 这个view分为上中下三部分 -->
 		<view class="more-share">
 			<view class="more-share-title u-f-ajc">分享到</view>
 			<scroll-view scroll-x class="more-share-body">
-				<view class="more-share-item" hover-class="more-share-hover">
-					<view class="icon iconfont icon-weixin u-f-ajc more-share-wx"></view>
-					<view>微信</view>
-				</view>
-				<view class="more-share-item" hover-class="more-share-hover">
-					<view class="icon iconfont icon-weixin u-f-ajc more-share-pyq"></view>
-					<view>朋友圈</view>
-				</view>
-				<view class="more-share-item" hover-class="more-share-hover">
-					<view class="icon iconfont icon-xinlangweibo u-f-ajc more-share-wb"></view>
-					<view>新浪微博</view>
-				</view>
-				<view class="more-share-item" hover-class="more-share-hover">
-					<view class="icon iconfont icon-QQ u-f-ajc more-share-qq"></view>
-					<view>QQ好友</view>
-				</view>
-				<view class="more-share-item" hover-class="more-share-hover">
-					<view class="icon iconfont icon-QQ u-f-ajc more-share-qq"></view>
-					<view>其它</view>
-				</view>
+				<block v-for="(val,index) in providerList" :key="index">
+					<view class="more-share-item" hover-class="more-share-hover" @tap="share(val)">
+						<view class="icon iconfont u-f-ajc" :class="['icon-'+val.zicon,'more-share-'+val.zclass]"></view>
+						<view>{{val.name}}</view>
+					</view>
+				</block>
 			</scroll-view>
 			<view class="more-share-bottom u-f-ajc" hover-class="more-share-hover" @tap="toggle">取消</view>
 		</view>
@@ -40,20 +28,201 @@
 		},
 		data() {
 			return {
-
-			};
+				shareText: 'uni-app可以同时发布成原生App、小程序、H5，邀请你一起体验！',
+				href: "https://uniapp.dcloud.io",
+				image: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/app/share-logo@3.png',
+				shareType: 1, //1文字 2图片 0图文 5小程序
+				providerList: []
+			}
+		},
+		onLoad() {
+			uni.getProvider({
+				service: 'share',
+				success: (e) => {
+					let data = []
+					for (let i = 0; i < e.provider.length; i++) {
+						switch (e.provider[i]) {
+							case 'weixin':
+								data.push({
+									name: '微信好友',
+									id: 'weixin',
+									zicon: "weixin",
+									zclass: "wx",
+									sort: 0
+								})
+								data.push({
+									name: '朋友圈',
+									id: 'weixin',
+									type: 'WXSenceTimeline',
+									zicon: "weixin",
+									zclass: "pyq",
+									sort: 1
+								})
+								break;
+							case 'sinaweibo':
+								data.push({
+									name: '新浪微博',
+									id: 'sinaweibo',
+									zicon: "xinlangweibo",
+									zclass: "wb",
+									sort: 2
+								})
+								break;
+							case 'qq':
+								data.push({
+									name: 'QQ好友',
+									id: 'qq',
+									zicon: "QQ",
+									zclass: "qq",
+									sort: 3
+								})
+								break;
+							default:
+								break;
+						}
+					}
+					this.providerList = data.sort((x, y) => {
+						return x.sort - y.sort
+					});
+				},
+				fail: (e) => {
+					console.log('获取分享通道失败', e);
+					uni.showModal({
+						content: '获取分享通道失败',
+						showCancel: false
+					})
+				}
+			});
 		},
 		methods: {
 			toggle() {
-				this.$emit('toggle');
+				this.$emit('toggle')
+			},
+			async share(e) {
+				console.log('分享通道:' + e.id + '； 分享类型:' + this.shareType);
+
+				if (!this.shareText && (this.shareType === 1 || this.shareType === 0)) {
+					uni.showModal({
+						content: '分享内容不能为空',
+						showCancel: false
+					})
+					return;
+				}
+
+				if (!this.image && (this.shareType === 2 || this.shareType === 0)) {
+					uni.showModal({
+						content: '分享图片不能为空',
+						showCancel: false
+					})
+					return;
+				}
+
+				let shareOPtions = {
+					provider: e.id,
+					scene: e.type && e.type === 'WXSenceTimeline' ? 'WXSenceTimeline' : 'WXSceneSession', //WXSceneSession”分享到聊天界面，“WXSenceTimeline”分享到朋友圈，“WXSceneFavorite”分享到微信收藏     
+					type: this.shareType,
+					success: (e) => {
+						console.log('success', e);
+						uni.showModal({
+							content: '分享成功',
+							showCancel: false
+						})
+					},
+					fail: (e) => {
+						console.log('fail', e)
+						uni.showModal({
+							content: e.errMsg,
+							showCancel: false
+						})
+					},
+					complete: function() {
+						console.log('分享操作结束!')
+					}
+				}
+
+				switch (this.shareType) {
+					case 0:
+						shareOPtions.summary = this.shareText;
+						shareOPtions.imageUrl = this.image;
+						shareOPtions.title = '欢迎体验uniapp';
+						shareOPtions.href = 'https://uniapp.dcloud.io';
+						break;
+					case 1:
+						shareOPtions.summary = this.shareText;
+						break;
+					case 2:
+						shareOPtions.imageUrl = this.image;
+						break;
+					case 5:
+						shareOPtions.imageUrl = this.image ? this.image :
+							'https://img-cdn-qiniu.dcloud.net.cn/uniapp/app/share-logo@3.png'
+						shareOPtions.title = '欢迎体验uniapp';
+						shareOPtions.miniProgram = {
+							id: 'gh_33446d7f7a26',
+							path: '/pages/tabBar/component/component',
+							webUrl: 'https://uniapp.dcloud.io',
+							type: 0
+						};
+						break;
+					default:
+						break;
+				}
+
+				if (shareOPtions.type === 0 && plus.os.name === 'iOS') { //如果是图文分享，且是ios平台，则压缩图片 
+					shareOPtions.imageUrl = await this.compress();
+				}
+				if (shareOPtions.type === 1 && shareOPtions.provider === 'qq') { //如果是分享文字到qq，则必须加上href和title
+					shareOPtions.href = 'https://uniapp.dcloud.io';
+					shareOPtions.title = '欢迎体验uniapp';
+				}
+				uni.share(shareOPtions);
+			},
+			compress() { //压缩图片 图文分享要求分享图片大小不能超过20Kb
+				console.log('开始压缩');
+				let img = this.image;
+				return new Promise((res) => {
+					var localPath = plus.io.convertAbsoluteFileSystem(img.replace('file://', ''));
+					console.log('after' + localPath);
+					// 压缩size
+					plus.io.resolveLocalFileSystemURL(localPath, (entry) => {
+						entry.file((file) => { // 可通过entry对象操作图片 
+							console.log('getFile:' + JSON.stringify(file));
+							if (file.size > 20480) { // 压缩后size 大于20Kb
+								plus.zip.compressImage({
+									src: img,
+									dst: img.replace('.jpg', '2222.jpg').replace('.JPG', '2222.JPG'),
+									width: '10%',
+									height: '10%',
+									quality: 1,
+									overwrite: true
+								}, (event) => {
+									console.log('success zip****' + event.size);
+									let newImg = img.replace('.jpg', '2222.jpg').replace('.JPG', '2222.JPG');
+									res(newImg);
+								}, function(error) {
+									uni.showModal({
+										content: '分享图片太大,需要请重新选择图片!',
+										showCancel: false
+									})
+								});
+							}
+						});
+					}, (e) => {
+						console.log('Resolve file URL failed: ' + e.message);
+						uni.showModal({
+							content: '分享图片太大,需要请重新选择图片!',
+							showCancel: false
+						})
+					});
+				})
 			}
-		},
+		}
 	}
 </script>
 
 <style scoped>
 	/* 分享到功能 */
-	
+
 	/* 蒙版class */
 	.more-share-model {
 		background-color: rgba(51, 51, 51, 0.49);
