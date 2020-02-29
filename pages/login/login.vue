e78f1bdbd12cc140a6ff4c26b3b224372bafbd48
yangemail8 <yangemail8@gmail.com>
2020-02-29 21:45:16 +0800

    #110 is done.

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -6,30 +6,38 @@
 		<view class="icon iconfont icon-guanbi" @tap="back"></view>
 		<!-- 引入背景图 -->
 		<image class="loginhead" src="../../static/common/loginhead.png" mode="widthFix" lazy-load></image>
-		<!-- 输入昵称/手机号/邮箱、密码、登录，输入框和按钮 -->
+		<!-- 输入框+按钮 -->
 		<view class="body">
-			<!-- <input type="text" class="uni-input common-input" placeholder="昵称/手机号/邮箱" />
-			<view class="login-input-box">
-				<input type="text" class="uni-input common-input forget-input" placeholder="请输入密码" />
-				<view class="forget u-f-ajc loign-font-color">忘记密码</view>
-			</view> -->
-			<view class="login-input-box">
-				<view class="phone u-f-ajc">+86</view>
-				<input type="text" class="uni-input common-input phone-input" placeholder="手机号" />
-			</view>
-			<view class="login-input-box">
-				<input type="text" class="uni-input common-input forget-input" placeholder="请输入验证码" />
-				<view class="forget u-f-ajc loign-font-color yanzhengma">
-					<view class="u-f-ajc">获取验证码</view>
+			<!-- 输入昵称/手机号/邮箱、密码、登录，输入框和按钮登录 -->
+			<template v-if="!status">
+				<input type="text" class="uni-input common-input" placeholder="昵称/手机号/邮箱" v-model="username" />
+				<view class="login-input-box">
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入密码" v-model="password" />
+					<view class="forget u-f-ajc loign-font-color">忘记密码</view>
 				</view>
-			</view>
+			</template>
+
+			<!-- 手机验证码登录 -->
+			<template v-else>
+				<view class="login-input-box">
+					<view class="phone u-f-ajc">+86</view>
+					<input type="text" class="uni-input common-input phone-input" placeholder="手机号" v-model="phone" />
+				</view>
+				<view class="login-input-box">
+					<input type="text" class="uni-input common-input forget-input" placeholder="请输入验证码" v-model="checknum" />
+					<view class="forget u-f-ajc loign-font-color yanzhengma">
+						<view class="u-f-ajc">获取验证码</view>
+					</view>
+				</view>
+			</template>
 
 			<button type="primary" :loading="loading" class="user-set-btn" :class="{'user-set-btn-disable':disable}" @tap="submit"
 			 :disabled="disable">登录</button>
 		</view>
 		<!-- 登录状态切换 -->
-		<view class="login-status u-f-ajc login-padding login-font-color">
-			验证码登录<view class="icon iconfont icon-jinru login-font-color"></view>
+		<view class="login-status u-f-ajc login-padding login-font-color" @tap="changeStatus">
+			{{status?'账号密码登录':'验证码登录'}}
+			<view class="icon iconfont icon-jinru login-font-color"></view>
 		</view>
 		<!-- 第三方登录 -->
 		<view class="other-login-title u-f-ajc login-padding login-font-color">第三方登录</view>
@@ -54,11 +62,51 @@
 		},
 		data() {
 			return {
-				loading: false,
+				status: false, //false：当前是账号密码登录； true：手机验证码登录
 				disable: true,
+				loading: false,
+				username: "",
+				password: "",
+				phone: "",
+				checknum: ""
+			}
+		},
+		watch: {
+			username(val) {
+				this.onBtnChange();
+			},
+			password(val) {
+				this.onBtnChange();
+			},
+			phone(val) {
+				this.onBtnChange();
+			},
+			checknum(val) {
+				this.onBtnChange();
 			}
 		},
 		methods: {
+			// 初始化表单
+			initInput() {
+				this.username = ""
+				this.password = ""
+				this.phone = ""
+				this.checknum = ""
+			},
+			// 改变按钮的状态
+			onBtnChange() {
+				if ((this.username && this.password) /*账号密码登录*/ || (this.phone && this.checknum) /*手机验证码登录*/ ) {
+					this.disable = false;
+					return;
+				}
+				this.disable = true;
+			},
+			// 切换登录状态
+			changeStatus() {
+				// 切换状态时,清空表单
+				this.initInput()
+				this.status = !this.status;
+			},
 			// 返回上一步
 			back() {
 				console.log("返回上一步");
@@ -106,8 +154,9 @@
 	.login-input-box .forget-input {
 		padding-right: 150rpx;
 	}
-	
-	.login-input-box .forget, .login-input-box .phone {
+
+	.login-input-box .forget,
+	.login-input-box .phone {
 		position: absolute;
 		top: 0;
 		height: 100%;
@@ -127,11 +176,11 @@
 		font-weight: bold;
 	}
 
-	.login-input-box .phone-input { 
+	.login-input-box .phone-input {
 		padding-left: 100rpx;
 	}
-	
-	.yanzhengma > view {
+
+	.yanzhengma>view {
 		background: #EEEEEE;
 		border-radius: 10rpx;
 		font-size: 25rpx;
