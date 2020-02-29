a8239147edd944fc4e30079f5b9c078b9369581b
yangemail8 <yangemail8@gmail.com>
2020-02-29 11:27:21 +0800

    #102 is done.

--- pages/user-set-userinfo/user-set-userinfo.vue
+++ pages/user-set-userinfo/user-set-userinfo.vue
@@ -10,14 +10,14 @@
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>昵称</view>
 			<view class="u-f-ac">
-				<view>昵称</view>
+				<input type="text" v-model="username" />
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>性别</view>
-			<view class="u-f-ac">
-				<view>不限</view>
+			<view class="u-f-ac" @tap="changeOne('sex')">
+				<view>{{sex}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
@@ -30,15 +30,15 @@
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>情感</view>
-			<view class="u-f-ac">
-				<view>已婚</view>
+			<view class="u-f-ac" @tap="changeOne('qg')">
+				<view>{{qg}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
 		<view class="user-set-userinfo-list u-f-ac u-f-jsb">
 			<view>职业</view>
-			<view class="u-f-ac">
-				<view>IT</view>
+			<view class="u-f-ac" @tap="changeOne('job')">
+				<view>{{job}}</view>
 				<view class="icon iconfont icon-bianji1"></view>
 			</view>
 		</view>
@@ -54,12 +54,19 @@
 </template>
 
 <script>
+	let sexArr = ["不限", "男", "女"];
+	let qgArr = ['秘密', '未婚', '已婚'];
+	let jobArr = ['秘密', 'IT', '老师'];
+
 	export default {
 		data() {
 			return {
 				// 后期此头像需要从服务器进行获取（当前登录用户的头像）
-				userpic:"../../static/demo/userpic/11.jpg",
-				
+				userpic: "../../static/demo/userpic/11.jpg",
+				username: "",
+				sex: "不限",
+				qg: "未婚",
+				job: "IT",
 			}
 		},
 		methods: {
@@ -69,12 +76,48 @@
 			// 用户修改头像
 			changeimg() {
 				uni.chooseImage({
-					count:1,
-					sizeType:['compressed'],
+					count: 1,
+					sizeType: ['compressed'],
 					success: (res) => {
 						this.userpic = res.tempFilePaths[0];
 					}
 				})
+			},
+			// 单列选择
+			changeOne(val) {
+				let arr = [];
+
+				switch (val) {
+					case 'sex':
+						arr = sexArr;
+						break;
+					case 'qg':
+						arr = qgArr;
+						break;
+					case 'job':
+						arr = jobArr;
+						break;
+					default:
+						break;
+				}
+				uni.showActionSheet({
+					itemList: arr,
+					success: res => {
+						switch (val) {
+							case 'sex':
+								this.sex = arr[res.tapIndex];
+								break;
+							case 'qg':
+								this.qg = arr[res.tapIndex];
+								break;
+							case 'job':
+								this.job = arr[res.tapIndex];
+								break;
+							default:
+								break;
+						}
+					}
+				});
 			}
 		}
 	}
@@ -99,7 +142,10 @@
 		width: 80rpx;
 		height: 80rpx;
 		border-radius: 100%;
+	}
 
+	.user-set-userinfo-list>view:last-child>input {
+		text-align: right;
 	}
 
 	.user-set-userinfo-list>view:last-child>view:last-of-type {
